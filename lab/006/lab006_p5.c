e78e8acd667b7d068a24618699cd5d827e0f75c3
Hardik Sachan <sachanhardik@gmail.com>
2022-01-28 14:05:45 +0530

    lab 6 q 5 done

--- lab/006/lab006_p5.c
+++ lab/006/lab006_p5.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main()
 {
+    int max = INT_MIN;
+    int n;
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter a number: ");
+        scanf("%d", &n);
+
+        max = max > n ? max : n;
+    }
+
+    printf("The largest of five numbers entered is: %d\n", max);
 }
