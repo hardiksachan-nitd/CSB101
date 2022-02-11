b76530124423e7fd9720c2b0e13e0aa50ed4763e
Hardik Sachan <sachanhardik@gmail.com>
2022-02-11 14:43:26 +0530

    lab 006 q1 updated to have custom impl

--- lab/007/lab007_p1.c
+++ lab/007/lab007_p1.c
@@ -2,47 +2,40 @@
 
 #include <stdio.h>
 
-void displayOctal(int n) { printf("Octal\t\t: %o\n", n); }
+void changeBase(int n, int base) {
+  int result[100];
 
-void displayHex(int n) { printf("Hexadecimal\t: %X\n", n); }
-
-void displayBinary(int n) {
-  char oct[1000];
-  sprintf(oct, "%o", n);
-
-  printf("Binary\t\t: ");
-
-  long i = 0;
-  while (oct[i]) {
-    switch (oct[i]) {
-    case '0':
-      printf("000");
-      break;
-    case '1':
-      printf("001");
-      break;
-    case '2':
-      printf("010");
-      break;
-    case '3':
-      printf("011");
-      break;
-    case '4':
-      printf("100");
-      break;
-    case '5':
-      printf("101");
-      break;
-    case '6':
-      printf("110");
-      break;
-    case '7':
-      printf("111");
-      break;
-    }
+  int i = 0;
+  while (n > 0) {
+    result[i] = n % base;
+    n = n / base;
     i++;
   }
 
+  for (int j = i - 1; j >= 0; j--) {
+    if (result[j] <= 9) {
+      printf("%d", result[j]);
+    } else {
+      printf("%c", 'A' + result[j] - 10);
+    }
+  }
+}
+
+void displayOctal(int n) {
+  printf("Octal\t\t: ");
+  changeBase(n, 8);
+  printf("\n");
+}
+
+void displayHex(int n) {
+  printf("Hexadecimal\t: ");
+  changeBase(n, 16);
+  printf("\n");
+}
+
+void displayBinary(int n) {
+  printf("Binary\t\t: ");
+  changeBase(n, 2);
   printf("\n");
 }
 
