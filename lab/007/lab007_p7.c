2d99f347f194f398e48ad0b421581cccef84efe3
Hardik Sachan <sachanhardik@gmail.com>
2022-02-17 19:54:54 +0530

    lab 007 q7 done

--- lab/007/lab007_p7.c
+++ lab/007/lab007_p7.c
@@ -7,11 +7,18 @@ int pow_r(int n, int r, int ans) {
   return pow_r(n, r-1, ans * n);
 }
 
+int pow_nr(int n, int r) {
+  int result =1;
+  for (int i = 1; i <= n; i++) {
+    result *= i;  
+  }
+}
+
 double power(int n, int r) {
   if (r > 0) {
     return pow_r(n, r, 1);
   }
-  return 1.0 / pow_r(n, -r, 1);
+  return 1.0 / pow_nr(n, -r);
 }
 
 int main() {
