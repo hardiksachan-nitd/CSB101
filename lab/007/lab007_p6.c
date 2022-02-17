eec3823a453864dd539d80723df334021c51789c
Hardik Sachan <sachanhardik@gmail.com>
2022-02-17 23:10:01 +0530

    lab 007 q6 done

---
+++ lab/007/lab007_p6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int number;
+
+void rev(int number) {
+  static int reverse = 0;
+  int remainder, sum = 0;
+  while (number != 0) {
+    remainder = number % 10;
+    sum += remainder;
+    reverse = reverse * 10 + remainder;
+    number /= 10;
+  }
+
+  printf("Reversed number = %d\n", reverse);
+  printf("Sum of Digits = %d\n", sum);
+}
+
+int main() {
+  printf("Enter an integer: ");
+  scanf("%d", &number);
+
+  rev(number);
+  return 0;
+}
