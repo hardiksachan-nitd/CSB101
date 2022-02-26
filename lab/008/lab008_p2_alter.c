ebb988493f331b4ec527018f10051867dccd7f42
Hardik Sachan <sachanhardik@gmail.com>
2022-02-26 16:47:16 +0530

    lab 8 q2 alter

---
+++ lab/008/lab008_p2_alter.c
@@ -0,0 +1,31 @@
+// display the sum and average of COVID-19 cases in Delhi for a week
+
+#include <stdio.h>
+
+int main() {
+  int arr[7][4];
+
+  for (int i = 0; i < 7; i++) {
+    scanf("%d/%*s %d %*s", &arr[i][0]);
+    scanf("%*s %d %*s", &arr[i][1]);
+    scanf("%*s %*s %d %*s", &arr[i][2]);
+    scanf("%*s %*s %d %*s", &arr[i][3]);
+  }
+
+  int hospitalisedSum = 0;
+  int recoveredSum = 0;
+  int deceasedSum = 0;
+
+  printf("Date\t\tHospitalized\tRecovered\tDeceased\n");
+  for (int i = 0; i < 7; i++) {
+    hospitalisedSum += arr[i][1];
+    recoveredSum += arr[i][2];
+    deceasedSum += arr[i][3];
+
+    printf("%d/01/2021\t%d\t\t%d\t\t%d\n",arr[i][0], arr[i][1], arr[i][2], arr[i][3]);
+  }
+  printf("%80s\n", "");
+  printf("Sum\t\t%d\t\t%d\t\t%d\n", hospitalisedSum, recoveredSum, deceasedSum);
+  printf("Average\t\t%f\t%f\t%f\n", hospitalisedSum / 7.0, recoveredSum / 7.0,
+         deceasedSum / 7.0);
+}
